3cd568be28dac0e6e082491ee8a0eb9e8e3cbd63
Marcus Pang <marcuspangyuyang@gmail.com>
2022-06-11 20:11:03 +0800

    Remove out folder and disable next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? "/out/": '',
-  basePath: isProd ? "/out" : "",
-  env: {
-    BACKEND_URL: "/out",
-  },
+  // assetPrefix: isProd ? "/out/": '',
+  // basePath: isProd ? "/out" : "",
+  // env: {
+  //   BACKEND_URL: "/out",
+  // },
 };
