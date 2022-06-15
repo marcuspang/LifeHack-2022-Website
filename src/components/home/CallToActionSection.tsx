6bf08c6d7d4d2d228c7e34cd7b3de34d11bf7d10
Marcus Pang <marcuspangyuyang@gmail.com>
2022-06-15 18:11:47 +0800

    Add deadline below register button

--- src/components/home/CallToActionSection.tsx
+++ src/components/home/CallToActionSection.tsx
@@ -1,4 +1,4 @@
-import { Button, Link, Text } from '@chakra-ui/react';
+import { Button, Link, Stack, Text } from '@chakra-ui/react';
 import Section from '../common/Section';
 
 const CallToActionSection = () => {
@@ -7,20 +7,27 @@ const CallToActionSection = () => {
       <Text fontWeight={600} fontSize="2xl" color="gray.300" mb={2}>
         Make sure to fill up this form below too!
       </Text>
-      <Button
-        as={Link}
-        p={6}
-        onClick={() => {
-          window.location.href = 'https://nus.campuslabs.com/engage/submitter/form/start/545156';
-        }}
-        variant="cta"
-        fontSize={['lg', 'xl', 'xl']}
-        mt={3}
-        colorScheme="blue"
-        size="lg"
-      >
-        Registration Form
-      </Button>
+      <Stack>
+        <Button
+          as={Link}
+          p={6}
+          onClick={() => {
+            window.location.href = 'https://nus.campuslabs.com/engage/submitter/form/start/545156';
+          }}
+          variant="cta"
+          fontSize={['lg', 'xl', 'xl']}
+          mt={3}
+          colorScheme="blue"
+          size="lg"
+          width={['auto', '300px', '300px', '300px']}
+          mx="auto"
+        >
+          Registration Form
+        </Button>
+        <Text as="span" color="gray.400" mt={2}>
+          Deadline: 27th June
+        </Text>
+      </Stack>
     </Section>
   );
 };
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -3,20 +3,30 @@ import { Box, Button, Center, Flex, Heading, Image, Link, Stack, Text } from '@c
 const Hero = () => {
   return (
     <Center bg="black" p={12} flexDir={['column', 'column', 'row']}>
-      <Image boxSize={['auto', 'auto', 'lg']} src="/logo.png" alt="LifeHack 2022 Logo" />
+      <Image boxSize={['auto', 'sm', 'md', 'lg']} src="/logo.png" alt="LifeHack 2022 Logo" />
       <Stack py={5} pl={[0, 8, 8]} spacing={1}>
-        <Heading as="h1" size={['2xl', '4xl', '4xl']} textAlign={['center', 'center', 'inherit']}>
+        <Heading
+          as="h1"
+          size={['2xl', '4xl', '4xl']}
+          textAlign={['center', 'inherit', 'inherit', 'inherit']}
+        >
           LifeHack 2022
         </Heading>
         <Box>
-          <Text fontSize={['lg', 'xl', '2xl']} textAlign={['center', 'center', 'inherit']}>
+          <Text
+            fontSize={['lg', 'xl', '2xl']}
+            textAlign={['center', 'center', 'inherit', 'inherit']}
+          >
             9th - 10th July & 16th July 2022
           </Text>
-          <Text fontSize={['md', 'lg', 'xl']} textAlign={['center', 'center', 'inherit']}>
+          <Text
+            fontSize={['md', 'lg', 'xl']}
+            textAlign={['center', 'center', 'inherit', 'inherit']}
+          >
             Stay ahead of change. Innovate the future.
           </Text>
         </Box>
-        <Flex justifyContent={['center', 'center', 'inherit']}>
+        <Flex alignItems={['center', 'center', 'inherit', 'inherit']} direction="column">
           <Button
             as={Link}
             p={6}
@@ -29,9 +39,13 @@ const Hero = () => {
             mt={3}
             colorScheme="blue"
             size="lg"
+            width="200px"
           >
             Register Now
           </Button>
+          <Text as="span" color="gray.400" mt={2}>
+            Deadline: 27th June!
+          </Text>
         </Flex>
       </Stack>
     </Center>
